b5f93642ea9a4a93d52e5b3f8905e305398e80bc
Fernando Glingani <fernando.glingani@gmail.com>
2021-01-16 18:10:56 -0300

    initial version

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as Express from 'express'
+
+const app = Express()
+
+app.get('/', (req : Express.Request, res : Express.Response) => {
+    res.send('test')
+})
+
+app.listen(8080, err => {
+    console.log('running')
+})
